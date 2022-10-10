44eff54b39709608fdadba64925b7980cf4df78a
Lívia Silva Santos <liviasantos@luizalabs.com>
2022-10-10 14:11:21 -0300

    add producer

---
+++ producer/src/main/java/com/labs/encontrotecnico/kafka/producer/ProducerApplication.java
@@ -0,0 +1,13 @@
+package com.labs.encontrotecnico.kafka.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApplication.class, args);
+	}
+
+}
---
+++ producer/src/main/java/com/labs/encontrotecnico/kafka/producer/gateway/http/ProducerController.java
@@ -0,0 +1,36 @@
+package com.labs.encontrotecnico.kafka.producer.gateway.http;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class ProducerController {
+    private final KafkaTemplate<String, String> producer;
+
+    @PostMapping("/topics/{topic}/messages")
+    public void sendToTopic(@PathVariable String topic, @RequestBody String message) {
+        producer.send(topic, message);
+    }
+
+    @PostMapping("/topics/{topic}/keys/{key}/messages")
+    public void sendToTopicAtKey(@PathVariable String topic,
+                                 @PathVariable String key,
+                                 @RequestBody String message) {
+        producer.send(topic, key, message);
+    }
+
+    @PostMapping("/topics/{topic}/keys/{key}/partitions/{partition}/messages")
+    public void sendToTopicAtKeyAndPartition(@PathVariable String topic,
+                                             @PathVariable String key,
+                                             @PathVariable Integer partition,
+                                             @RequestBody String message) {
+        producer.send(topic, partition, key, message);
+    }
+}
